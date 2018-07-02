f2550bab18cd1a000bf91b6eff5bab810f301fa6
Weidong Zhang <battle975444818@outlook.com>
2018-07-02 18:03:14 +0800

    fix bug

--- src/caffe/util/bbox_util.cpp
+++ src/caffe/util/bbox_util.cpp
@@ -1991,33 +1991,13 @@ void EncodePosePrediction(const Dtype* pose_data, const int num,
       const vector<vector<int> >& all_neg_indices,
       const map<int, vector<NormalizedBBox> >& all_gt_bboxes,
       Dtype* pose_pred_data, Dtype* pose_gt_data) {
-  // CHECK_EQ(num, all_match_indices.size());
-  // CHECK_EQ(num, all_neg_indices.size());
-  // Retrieve parameters.
-  //CHECK(multibox_loss_param.has_num_classes()) << "Must provide num_classes.";
-  // const int num_classes = multibox_loss_param.num_classes();
-  // CHECK_GE(num_classes, 1) << "num_classes should not be less than 1.";
-  // const int background_label_id = multibox_loss_param.background_label_id();
+
   const int aspect_classes = 4;
-  // const bool map_object_to_agnostic =
-  //     multibox_loss_param.map_object_to_agnostic();
-  // if (map_object_to_agnostic) {
-  //   if (background_label_id >= 0) {
-  //     CHECK_EQ(num_classes, 2);
-  //   } else {
-  //     CHECK_EQ(num_classes, 1);
-  //   }
-  // }
+
   const MiningType mining_type = multibox_loss_param.mining_type();
   bool do_neg_mining;
   do_neg_mining = false;
   // we dont do negative mining in pose prediction
-  // if (multibox_loss_param.has_do_neg_mining()) {
-  //   LOG(WARNING) << "do_neg_mining is deprecated, use mining_type instead.";
-  //   do_neg_mining = multibox_loss_param.do_neg_mining();
-  //   CHECK_EQ(do_neg_mining,
-  //            mining_type != MultiBoxLossParameter_MiningType_NONE);
-  // }
   //do_neg_mining = mining_type != MultiBoxLossParameter_MiningType_NONE;
   const ConfLossType pose_loss_type = multibox_loss_param.pose_loss_type();
   int count = 0;
@@ -2045,44 +2025,8 @@ void EncodePosePrediction(const Dtype* pose_data, const int num,
             default:
               LOG(FATAL) << "Unknown pose loss type.";
           }
-          // if (do_neg_mining) {
-          //   // Copy scores for matched bboxes.
-          //   caffe_copy<Dtype>(num_classes, conf_data + j * num_classes,
-          //       conf_pred_data + count * num_classes);
-          //   ++count;
-          // }
         }
       }
-      // Go to next image.
-    //   if (do_neg_mining) {
-    //     // Save negative bboxes scores and labels.
-    //     for (int n = 0; n < all_neg_indices[i].size(); ++n) {
-    //       int j = all_neg_indices[i][n];
-    //       CHECK_LT(j, num_priors);
-    //       caffe_copy<Dtype>(num_classes, conf_data + j * num_classes,
-    //           conf_pred_data + count * num_classes);
-    //       switch (conf_loss_type) {
-    //         case MultiBoxLossParameter_ConfLossType_SOFTMAX:
-    //           conf_gt_data[count] = background_label_id;
-    //           break;
-    //         case MultiBoxLossParameter_ConfLossType_LOGISTIC:
-    //           if (background_label_id >= 0 &&
-    //               background_label_id < num_classes) {
-    //             conf_gt_data[count * num_classes + background_label_id] = 1;
-    //           }
-    //           break;
-    //         default:
-    //           LOG(FATAL) << "Unknown conf loss type.";
-    //       }
-    //       ++count;
-    //     }
-    //   }
-    // }
-    // if (do_neg_mining) {
-    //   conf_data += num_priors * num_classes;
-    // } else {
-    //   conf_gt_data += num_priors;
-    // }
     pose_gt_data += num_priors;
   }
 }
